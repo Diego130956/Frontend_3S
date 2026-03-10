c19986dbeabaea8c8cab85ffcc3d16ad04acb6c9
Diego130956 <diegobertaglia450@gmail.com>
2026-03-10 08:14:02 -0300

    init

--- static/js/apis.js
+++ static/js/apis.js
@@ -17,3 +17,45 @@ function render_gato(dados) {
         ImgGato.style.display = "block"
         ImgGato.src = urlImg
 }
+
+
+
+async function get_cachorro() {
+    let resultado = await fetch("https://dog.ceo/api/breeds/image/random")
+
+    if (resultado.ok) {
+        let dados = await resultado.json()
+        render_cachorro(dados)
+    }
+
+}
+
+function render_cachorro(dados) {
+        let urlImg = dados.message
+        const ImgCachorro = document.getElementById("img-cachorro")
+        const IconCachorro = document.getElementById("icon-cachorro")
+
+        IconCachorro.style.display = "none"
+        ImgCachorro.style.display = "block"
+        ImgCachorro.src = urlImg
+}
+
+async function get_raposa() {
+    let resultado = await fetch("https://randomfox.ca/floof")
+
+    if (resultado.ok) {
+        let dados = await resultado.json()
+        render_raposa(dados)
+    }
+
+}
+
+function render_raposa(dados) {
+        let urlImg = dados.image
+        const ImgRaposa = document.getElementById("img-raposa")
+        const IconRaposa = document.getElementById("icon-raposa")
+
+        IconRaposa.style.display = "none"
+        ImgRaposa.style.display = "block"
+        ImgRaposa.src = urlImg
+}
